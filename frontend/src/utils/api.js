6093f2cc4ed3ea3ee8d15d1432576a9d00efd133
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-07-11 19:01:35 +0300

    fixed an error in frontend api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.dasha.nomorepartiesco.ru/');
+  const api = new Api('https://api.dasha.nomorepartiesco.ru');
 
   export default api;
